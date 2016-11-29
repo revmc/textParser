aa13d5372045f1898343612dcff8c2260ecae04b
revmc <shaque9000@gmail.com>
2016-11-29 09:38:38 -0500

    First commit for a code challenge related to my interview process with COTA

---
+++ scripts/textParser.js
@@ -0,0 +1,157 @@
+window.textParser = window.textParser || {};
+
+(function() {
+
+	function getTimeDuration(paragraphText) {
+
+		var datePattern = /(0[1-9]|1[0-2])[\/](0[1-9]|[12]\d|3[01])[\/](19|20)\d{2}/g;
+
+		var dateMatchArray = paragraphText.match(datePattern);
+
+		var diffDays = 0;
+
+		if (dateMatchArray) {
+
+			// convert text dates to date objects
+			var arrDates = dateMatchArray.map(function(value, index, arr) {
+				return new Date(value);
+			});
+
+			// sort date array into ascending order
+			arrDates.sort(function (date1, date2) {
+				if (date1 > date2) return 1;
+				if (date1 < date2) return -1;
+				return 0;
+			});
+
+			var earliestDate = arrDates[0]; // first array element
+			var latestDate = arrDates[(arrDates.length - 1)]; // last array element
+
+			var timeDiff = Math.abs(latestDate.getTime() - earliestDate.getTime());
+
+			diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1; // + 1 because this is inclusive of the given dates
+
+		}
+
+		return diffDays;
+
+	}
+
+	window.textParser.parseText = function(inputJSON) {
+
+		var inputObject = JSON.parse(inputJSON);
+
+		var paragraphText = inputObject.paragraph;
+
+		var diffDays = getTimeDuration(paragraphText);
+
+		// START DATE PARSING
+
+		// var datePattern = /(0[1-9]|1[0-2])[\/](0[1-9]|[12]\d|3[01])[\/](19|20)\d{2}/g;
+
+		// var dateMatchArray = paragraphText.match(datePattern);
+
+		// var diffDays = 0;
+
+		// if (dateMatchArray) {
+
+		// 	// convert text dates to date objects
+		// 	var arrDates = dateMatchArray.map(function(value, index, arr) {
+		// 		return new Date(value);
+		// 	});
+
+		// 	// sort date array into ascending order
+		// 	arrDates.sort(function (date1, date2) {
+		// 		if (date1 > date2) return 1;
+		// 		if (date1 < date2) return -1;
+		// 		return 0;
+		// 	});
+
+		// 	var earliestDate = arrDates[0]; // first array element
+		// 	var latestDate = arrDates[(arrDates.length - 1)]; // last array element
+
+		// 	var timeDiff = Math.abs(latestDate.getTime() - earliestDate.getTime());
+
+		// 	diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1; // + 1 because this is inclusive of the given dates
+
+		// }
+
+		// END DATE PARSING
+
+		// START GENDER PARSING
+
+		var gender = 'unknown';
+
+		var genderPattern = /\bhe\b|\bshe\b/ig;
+
+		var genderMatchArray = paragraphText.match(genderPattern);
+
+		var numMales = 0,
+			numFemales = 0;
+
+		if (genderMatchArray) {
+
+			var numPeople = genderMatchArray.length;
+
+			for (var i = 0; i < numPeople; ++i) {
+				
+				if (genderMatchArray[i] === 'he') {
+					numMales++;
+				}
+
+			}
+
+			var numFemales = numPeople - numMales;
+
+			if (numMales > numFemales) {
+				gender = 'male';
+			} else if (numFemales > numMales) {
+				gender = 'female';
+			}
+
+		}
+
+		// END GENDER PARSING
+
+		// START SENTIMENT PARSING
+
+		var sentiment = 'unknown';
+
+		var positiveSentimentPattern = /happy|glad|jubilant|satisfied/gi; 
+		var negativeSentimentPattern = /sad|disappointed|angry|frustrated/gi;
+
+		// get arrays of matching sentiments, one array for each type of sentiment
+		var positiveSentimentMatchArray = paragraphText.match(positiveSentimentPattern);
+		var negativeSentimentMatchArray = paragraphText.match(negativeSentimentPattern);
+
+		var numPositiveSentiments = positiveSentimentMatchArray ? positiveSentimentMatchArray.length : 0;
+		var numNegativeSentiments = negativeSentimentMatchArray ? negativeSentimentMatchArray.length : 0;
+
+		if (numPositiveSentiments > numNegativeSentiments) {
+			sentiment = 'positive';
+		} else if (numNegativeSentiments > numPositiveSentiments) {
+			sentiment = 'negative';
+		} else if (numPositiveSentiments === numNegativeSentiments) {
+			sentiment = 'mixed';
+		}
+
+		// END SENTIMENT PARSING
+
+		
+		var returnObj = {
+			timeDuration: diffDays,
+			gender: gender,
+			sentiment: sentiment
+		};
+
+		var returnJSON = JSON.stringify(returnObj);
+
+		return(returnJSON);
+
+	};
+
+	window.textParser.init = function() {
+		document.getElementById('add').addEventListener('click', parseText);
+	};
+
+})();
---
+++ scripts/textParser.test.js
@@ -0,0 +1,43 @@
+describe('TextParser', function() {
+
+	// Example 1 tests
+
+	it('should return the correct data points for example 1', function() {
+
+		var example1TextObject = { 
+			paragraph: 'John downloaded the Pokemon Go app on 07/15/2016. By 07/22/2016, he was on level 24. Initially, he was very happy with the app. However, he soon became very disappointed with the app because it was crashing very often. As soon as he reached level 24, he uninstalled the app.'
+		};
+
+		var example1JSONObject = JSON.stringify(example1TextObject);
+
+		var textParserReturn = textParser.parseText(example1JSONObject);
+
+		expect(JSON.parse(textParserReturn).timeDuration).toBe(8);
+
+		expect(JSON.parse(textParserReturn).gender).toBe('male');
+
+		expect(JSON.parse(textParserReturn).sentiment).toBe('mixed');
+
+	});
+
+	// Example 2 tests
+	
+	it('should return the correct data points for example 2', function() {
+
+		var example2TextObject = { 
+			paragraph: 'Hua Min liked playing tennis. She first started playing on her 8th birthday - 07/07/1996. Playing tennis always made her happy. She won her first tournament on 08/12/2010. However, on 04/15/2015 when she was playing at the Flushing Meadows, she had a serious injury and had to retire from her tennis career.'
+		};
+
+		var example2JSONObject = JSON.stringify(example2TextObject);
+
+		var textParserReturn = textParser.parseText(example2JSONObject);
+
+		expect(JSON.parse(textParserReturn).timeDuration).toBe(6857);
+
+		expect(JSON.parse(textParserReturn).gender).toBe('female');
+
+		expect(JSON.parse(textParserReturn).sentiment).toBe('positive');
+
+	});
+
+});
